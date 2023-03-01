9927f2ea05e78186a8f3e0e8e01323f5b48b6e01
Xb0rg <101411376+Xb0rg@users.noreply.github.com>
2023-03-01 22:53:32 +0800

    First code Test Update
    Testing Github repository

---
+++ namespace.py
@@ -0,0 +1,19 @@
+# scopes1.py
+# Local versus Global
+# we define a function, called local
+def local():
+    m = 7
+    print(m)
+m = 5
+print(m)
+# we call, or `execute` the function local
+local()
+
+def local():
+ # m doesn't belong to the scope defined by the local function
+ # so Python will keep looking into the next enclosing scope.
+ # m is finally found in the global scope
+    print(m, 'printing from the local scope')
+m = 5
+print(m, 'printing from the global scope')
+local()
